899b6a95bb7a91e9a8bb4b8aa34158fd213fb754
Alexander Ilseman <al@ilseman.com>
2024-06-09 08:11:10 -0500

    Transparent background for tokyonight

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent", -- style for sidebars, see below
+        floats = "transparent",
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-night",
+    },
+  },
+}
