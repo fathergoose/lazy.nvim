c95b9d50ef1fdfa69403fddde12bb07f5370db8e
Alexander Ilseman <al@ilseman.com>
2024-06-24 14:05:22 -0500

    updates

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.api.nvim_set_keymap("", "<leader>mdx", "<cmd>lua require('config/functions').export_md_to_clipboard()<CR>", {})
+-- Ctrl-space
+vim.api.nvim_set_keymap("", "<C-SPACE>", "<cmd>lua require('config/functions').toggle_copilot()<CR>", {})
