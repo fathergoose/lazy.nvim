6a1220c1e12ffe949f96a5ac0cc0e9cdf1be2e30
Alexander Ilseman <alexander.ilseman@gmail.com>
2024-07-12 08:25:31 -0500

    Got gitui working

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -2,6 +2,40 @@
 return {
   { "darfink/vim-plist" },
   {
+    "williamboman/mason.nvim",
+    opts = { ensure_installed = { "gitui" } },
+    keys = {
+      {
+        "<leader>gG",
+        function()
+          LazyVim.terminal.open({ "/usr/local/bin/gitui" }, { esc_esc = false, ctrl_hjkl = false })
+        end,
+        desc = "GitUi (cwd)",
+      },
+      {
+        "<leader>gg",
+        function()
+          LazyVim.terminal.open(
+            { "/usr/local/bin/gitui" },
+            { cwd = LazyVim.root.get(), esc_esc = false, ctrl_hjkl = false }
+          )
+        end,
+        desc = "GitUi (Root Dir)",
+      },
+    },
+    init = function()
+      -- delete lazygit keymap for file history
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "LazyVimKeymaps",
+        once = true,
+        callback = function()
+          pcall(vim.keymap.del, "n", "<leader>gf")
+          pcall(vim.keymap.del, "n", "<leader>gl")
+        end,
+      })
+    end,
+  },
+  {
     "ahmedkhalf/project.nvim",
     config = function()
       require("project_nvim").setup({
@@ -54,7 +88,7 @@ return {
       fluff.args = {
         "lint",
         "--dialect=postgres",
-        "-"
+        "-",
       }
     end,
   },
