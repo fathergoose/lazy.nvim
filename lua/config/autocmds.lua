f2e6a7fa597528adc8b54bcd140f9109c927d0e2
Alexander Ilseman <alexander.ilseman.com>
2024-07-16 18:59:46 -0500

    remove cruft

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,27 +2,6 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- vim.g.copilot_enabled = false
--- vim.cmd("Copilot disable")
---
--- vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
---   pattern = { "*" },
---   callback = function()
---     if vim.bo.filetype == "markdown" or vim.bo.filetype == "txt" then
---       vim.cmd("Copilot disable")
---     else
---       if vim.g.copilot_enabled then
---         vim.cmd("Copilot enable")
---       else
---         vim.cmd("Copilot disable")
---       end
---     end
---   end,
--- })
-
--- I want a variable to track enabled/diabled that defaults to disabled
--- but keeps it's state unless entering a markdown or text buffer
-
 vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
   pattern = { "*" },
   command = "silent! wa",
